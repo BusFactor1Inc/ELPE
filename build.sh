7befb74efe5fe5cdf0837541434678ae7166ff53
BusFactor1 Inc <BusFactor1@gmai.com>
2017-03-27 22:08:07 -0600

    Update to properly set version in Info.plist.

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
-build_number=${1?enter a build number}
+version=${1?enter a version}
 appname="Emacs Lisp Programming Environment (ELPE).app"
 
 rm -rf nextstep/"${appname}"
 make -j17 install
+
 
 cp -a nextstep/Emacs.app/ nextstep/"${appname}"/
 
 cp "ELPE.icns" "nextstep/${appname}/Contents/Resources/Emacs.icns"
 
 cp nextstep/Cocoa/Emacs.base/Contents/Info.plist nextstep/"${appname}"/Contents/Info.plist
+gsed -i "s|ELPE_VERSION|$version|" nextstep/"${appname}"/Contents/Info.plist
 
 xattr -cr nextstep/"${appname}"
 
